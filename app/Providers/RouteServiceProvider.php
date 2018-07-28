4a5e7282d5271d36cdf31829ea9e79500b24192c
root <admin@example.com>
2018-07-29 00:55:31 +0800

    wechatController

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,6 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapWechatRoutes();
         //
     }
 
@@ -69,5 +70,19 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * Define the "wechat" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWechatRoutes()
+    {
+        Route::prefix('wechat')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/wechat.php'));
     }
 }
---
+++ routes/wechat.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chendongdong
+ * Date: 2018/7/29
+ * Time: 0:36
+ */
+
+Route::any('/', 'Wechat\WeChatController@serve');
