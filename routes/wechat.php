6bf6187a0c990d6f67a2743c29d37da5481496b3
root <admin@example.com>
2018-07-29 21:43:29 +0800

    添加auth

--- routes/wechat.php
+++ routes/wechat.php
@@ -7,3 +7,11 @@
  */
 
 Route::any('/', 'Wechat\WeChatController@serve');
+
+Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
+    Route::get('/user', function () {
+        $user = session('wechat.oauth_user'); // 拿到授权用户资料
+
+        dd($user);
+    });
+});
