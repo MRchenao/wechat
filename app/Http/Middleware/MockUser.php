b8af4ac2bf7f8b88f42e9f2eb386f111f25d01e9
Administrator <admin@example.com>
2018-07-31 15:05:42 +0800

    xxx

--- app/Http/Middleware/MockUser.php
+++ app/Http/Middleware/MockUser.php
@@ -4,22 +4,20 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Overtrue\Socialite\User as SocialiteUser;
+use EasyWeChat\Factory;
+
 
 class MockUser
 {
     public function handle($request, Closure $next)
     {
+        $app = Factory::miniProgram(config('wechat.mini_program.default'));
+        $code = $request->header('x-wx-code');
+        $session = $app->auth->session($code);
+        return $session;
+        var_dump($session);exit;
 
-        $user = new SocialiteUser([
-            'id' => '12345',//openid
-            'name' => 'mock',
-            'nickname' => 'mock user',
-            'avatar' => '',
-            'email' => '12345@qq.com',
-            'original' => [],
-            'provider' => 'WeChat',
-        ]);
-        session(['wechat.oauth_user.default' => $user]);
+        session(['wechat.oauth_user.default' => $session]);
         return $next($request);
     }
 }
