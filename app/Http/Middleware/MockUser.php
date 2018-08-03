5ba921db5fe2a58529bbbab570ce2be29e2bb347
Administrator <admin@example.com>
2018-08-03 17:52:42 +0800

    登录处理

--- app/Http/Middleware/MockUser.php
+++ app/Http/Middleware/MockUser.php
@@ -3,21 +3,39 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Cache;
 use Overtrue\Socialite\User as SocialiteUser;
 use EasyWeChat\Factory;
 
 
 class MockUser
 {
+    protected $cacheTime = 86400;
+    protected $string = 'Fiasdjl8F9sajk9ASG23';
+
     public function handle($request, Closure $next)
     {
-        $app = Factory::miniProgram(config('wechat.mini_program.default'));
-        $code = $request->header('x-wx-code');
-        $session = $app->auth->session($code);
-        return $session;
-        var_dump($session);exit;
-
-        session(['wechat.oauth_user.default' => $session]);
+        $token = $request->header('token');
+        if (!Cache::get($token)) {
+            $app = Factory::miniProgram(config('wechat.mini_program.default'));
+            $code = $request->header('x-wx-code');
+            $session = $app->auth->session($code);
+            $user_info = $request->input('userInfo');
+            $openid = $session['openid'];
+            $key = $session['session_key'];
+            $user_info['openid'] = $openid;
+            /*        $data['name'] = $user_info['nickName'];
+                    $data['gender'] = $user_info['gender'];
+                    $data['city'] = $user_info['city'];
+                    $data['province'] = $user_info['province'];
+                    $data['country'] = $user_info['country'];
+                    $data['avatarUrl'] = $user_info['avatarUrl'];
+                    $data['openid'] = $session['openid'];*/
+            $user_info['session_key'] = $key;
+            $login_key = md5($openid . $key . $this->string);
+            Cache::put($login_key, $user_info, $this->cacheTime);
+            session(['wechat.oauth_user.default' => $user_info]);
+        }
         return $next($request);
     }
 }
